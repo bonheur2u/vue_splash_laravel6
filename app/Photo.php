3de78810271bd76941e8959ce70e75979b6e5101
bonheur2u <bono.tsukasa@gmail.com>
2021-03-17 08:14:29 +0900

    いいね機能

--- app/Photo.php
+++ app/Photo.php
@@ -4,15 +4,18 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth; 
 
 class Photo extends Model
 {
     /** プライマリキーの型 */
     protected $keyType = 'string';
 
-    /** JSONに含める属性 */
+    /** JSONに含めるアクセサ */
     protected $appends = [
         'url',
+        'likes_count',
+        'liked_by_user',
     ];
 
     // /** JSONに含めない属性 */
@@ -28,6 +31,8 @@ class Photo extends Model
         'owner',
         'url',
         'comments',
+        'likes_count',
+        'liked_by_user',
     ];
 
     /** IDの桁数 */
@@ -99,5 +104,38 @@ class Photo extends Model
         }
 
         return $id;
+    }
+
+    /**
+     * リレーションシップ - usersテーブル
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function likes()
+    {
+        return $this->belongsToMany('App\User', 'likes')->withTimestamps();
+    }
+
+    /**
+     * アクセサ - likes_count
+     * @return int
+     */
+    public function getLikesCountAttribute()
+    {
+        return $this->likes->count();
+    }
+
+    /**
+     * アクセサ - liked_by_user
+     * @return boolean
+     */
+    public function getLikedByUserAttribute()
+    {
+        if (Auth::guest()) {
+            return false;
+        }
+
+        return $this->likes->contains(function ($user) {
+            return $user->id === Auth::user()->id;
+        });
     }
 }
